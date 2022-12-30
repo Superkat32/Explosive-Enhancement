8fbe371c2713dae42d978c2a7d464b287645f95c
Superkat32 <superkat1806@gmail.com>
2022-12-30 00:23:41 -0500

    Added a linger particle, which... lingers after an explosion. Still need to work on the movement part of the particle, however.

--- src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
@@ -15,11 +15,13 @@ public class ExplosiveEnhancement implements ModInitializer {
 
 	public static final DefaultParticleType BOOM = FabricParticleTypes.simple();
 	public static final DefaultParticleType BIG_EXPLOSION = FabricParticleTypes.simple();
+	public static final DefaultParticleType LINGER = FabricParticleTypes.simple();
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "boom"), BOOM);
 		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "big_explosion"), BIG_EXPLOSION);
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "linger"), LINGER);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/superkat/explosiveenhancement/LingerParticle.java
@@ -0,0 +1,55 @@
+package net.superkat.explosiveenhancement;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.DefaultParticleType;
+
+@Environment(EnvType.CLIENT)
+public class LingerParticle  extends SpriteBillboardParticle {
+    private final SpriteProvider spriteProvider;
+
+    LingerParticle(ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, SpriteProvider spriteProvider) {
+        super(world, x, y, z);
+        this.spriteProvider = spriteProvider;
+        this.maxAge = this.random.nextInt(50) + 35;
+        this.scale = 1F;;
+//        this.gravityStrength = 0.008F;
+        this.gravityStrength = 3.0E-6F;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY + (double)(this.random.nextFloat() / 500.0F);
+        this.velocityZ = velocityZ;
+        this.setSpriteForAge(spriteProvider);
+    }
+
+    public void tick() {
+        this.prevPosX = this.x;
+        this.prevPosY = this.y;
+        this.prevPosZ = this.z;
+        if (this.age++ >= this.maxAge) {
+            this.markDead();
+        } else {
+            this.velocityY -= (double)this.gravityStrength;
+            this.move(this.velocityX, this.velocityY, this.velocityZ);
+            this.setSpriteForAge(this.spriteProvider);
+        }
+    }
+
+    public ParticleTextureSheet getType() {
+        return ParticleTextureSheet.PARTICLE_SHEET_TRANSLUCENT;
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static class Factory implements ParticleFactory<DefaultParticleType> {
+        private final SpriteProvider spriteProvider;
+
+        public Factory(SpriteProvider spriteProvider) {
+            this.spriteProvider = spriteProvider;
+        }
+
+        public Particle createParticle(DefaultParticleType defaultParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+            return new LingerParticle(clientWorld, d, e, f, g, h, i, this.spriteProvider);
+        }
+    }
+}
