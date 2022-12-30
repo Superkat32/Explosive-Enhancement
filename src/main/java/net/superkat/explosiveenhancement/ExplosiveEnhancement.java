e88ca080b329601a1390caffbe2c4ec209535c5f
Superkat32 <superkat1806@gmail.com>
2022-12-29 20:48:27 -0500

    Transferred everything from previous mod attempt over to new folder/repo.

--- src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
@@ -1,6 +1,11 @@
 package net.superkat.explosiveenhancement;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,8 +13,13 @@ public class ExplosiveEnhancement implements ModInitializer {
 	public static final String MOD_ID = "explosiveenhancement";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final DefaultParticleType BOOM = FabricParticleTypes.simple();
+	public static final DefaultParticleType BIG_EXPLOSION = FabricParticleTypes.simple();
+
 	@Override
 	public void onInitialize() {
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "boom"), BOOM);
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "big_explosion"), BIG_EXPLOSION);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancementClient.java
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancementClient.java
@@ -1,11 +1,15 @@
 package net.superkat.explosiveenhancement;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 
 public class ExplosiveEnhancementClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+
+        ParticleFactoryRegistry.getInstance().register(ExplosiveEnhancement.BOOM, BoomParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(ExplosiveEnhancement.BIG_EXPLOSION, BigExplosionParticle.Factory::new);
 
     }
 }
