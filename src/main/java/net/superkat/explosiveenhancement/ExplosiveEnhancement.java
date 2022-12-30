ef49e0283667fbb85a169f1a5cbdf32dc13ba8f8
Superkat32 <superkat1806@gmail.com>
2022-12-29 20:02:13 -0500

    Initial commit.

---
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
@@ -0,0 +1,15 @@
+package net.superkat.explosiveenhancement;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExplosiveEnhancement implements ModInitializer {
+	public static final String MOD_ID = "explosiveenhancement";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancementClient.java
@@ -0,0 +1,11 @@
+package net.superkat.explosiveenhancement;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExplosiveEnhancementClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
