5a0d39f373e255d82924b842e8ce300290235c7f
Superkat32 <superkat1806@gmail.com>
2023-01-07 23:22:39 -0500

    Created branch: 1.19.2.
    Backport to Minecraft version 1.19.2.
    Changes were made to gradle.properties.
    Fixed in-game link leading to the Fabric Example Mod. The bug still applies to 1.19.3, however.

--- src/main/java/net/superkat/explosiveenhancement/BoomParticle.java
+++ src/main/java/net/superkat/explosiveenhancement/BoomParticle.java
@@ -8,14 +8,16 @@ import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3d;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
+import net.minecraft.util.math.Vec3f;
+//import org.joml.Quaternion;
+//import org.joml.Vec3;
 
 @Environment(EnvType.CLIENT)
 public class BoomParticle extends SpriteBillboardParticle {
     private final SpriteProvider sprites;
-    private static final Quaternionf QUATERNION = new Quaternionf(0F, -0.7F, 0.7F, 0F);
+    private static final Quaternion QUATERNION = new Quaternion(0F, -0.7F, 0.7F, 0F);
 
     BoomParticle(ClientWorld world, double x, double y, double z, double velX, double velY, double velZ, SpriteProvider sprites) {
         super(world, x, y + 0.5, z, 0.0, 0.0, 0.0);
@@ -37,13 +39,13 @@ public class BoomParticle extends SpriteBillboardParticle {
         float y = (float) (MathHelper.lerp(ticks, this.prevPosY, this.y) - vec3.getY());
         float z = (float) (MathHelper.lerp(ticks, this.prevPosZ, this.z) - vec3.getZ());
 
-        Vector3f[] vector3fs = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
+        Vec3f[] vector3fs = new Vec3f[]{new Vec3f(-1.0F, -1.0F, 0.0F), new Vec3f(-1.0F, 1.0F, 0.0F), new Vec3f(1.0F, 1.0F, 0.0F), new Vec3f(1.0F, -1.0F, 0.0F)};
         float f4 = this.getSize(ticks);
 
         for(int i = 0; i < 4; ++i) {
-            Vector3f vector3f = vector3fs[i];
+            Vec3f vector3f = vector3fs[i];
             vector3f.rotate(QUATERNION);
-            vector3f.mul(f4);
+            vector3f.scale(f4);
             vector3f.add(x, y, z);
         }
 
@@ -52,10 +54,10 @@ public class BoomParticle extends SpriteBillboardParticle {
         float f5 = this.getMinV();
         float f6 = this.getMaxV();
         int light = this.getBrightness(ticks);
-        buffer.vertex(vector3fs[0].x(), vector3fs[0].y(), vector3fs[0].z()).texture(f8, f6).color(this.red, this.green, this.blue, this.alpha).light(light).next();
-        buffer.vertex(vector3fs[1].x(), vector3fs[1].y(), vector3fs[1].z()).texture(f8, f5).color(this.red, this.green, this.blue, this.alpha).light(light).next();
-        buffer.vertex(vector3fs[2].x(), vector3fs[2].y(), vector3fs[2].z()).texture(f7, f5).color(this.red, this.green, this.blue, this.alpha).light(light).next();
-        buffer.vertex(vector3fs[3].x(), vector3fs[3].y(), vector3fs[3].z()).texture(f7, f6).color(this.red, this.green, this.blue, this.alpha).light(light).next();
+        buffer.vertex(vector3fs[0].getX(), vector3fs[0].getY(), vector3fs[0].getZ()).texture(f8, f6).color(this.red, this.green, this.blue, this.alpha).light(light).next();
+        buffer.vertex(vector3fs[1].getX(), vector3fs[1].getY(), vector3fs[1].getZ()).texture(f8, f5).color(this.red, this.green, this.blue, this.alpha).light(light).next();
+        buffer.vertex(vector3fs[2].getX(), vector3fs[2].getY(), vector3fs[2].getZ()).texture(f7, f5).color(this.red, this.green, this.blue, this.alpha).light(light).next();
+        buffer.vertex(vector3fs[3].getX(), vector3fs[3].getY(), vector3fs[3].getZ()).texture(f7, f6).color(this.red, this.green, this.blue, this.alpha).light(light).next();
     }
 
     @Override
