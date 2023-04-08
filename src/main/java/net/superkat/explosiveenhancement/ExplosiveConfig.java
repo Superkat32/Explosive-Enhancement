5156703bf910c8a46919872e12f36f45d3fa24b1
Superkat32 <superkat1806@gmail.com>
2023-04-07 22:47:02 -0400

    Updated config
    FEAT: New underwater enum config
    REFACTOR: Updated the look of the config
    
    I'm still debating if I should switch from MidnightLib to YACL. YACL will allow me to utilize tabs in 1.19.2, while MidnightLib is much easier to use...

--- src/main/java/net/superkat/explosiveenhancement/ExplosiveConfig.java
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveConfig.java
@@ -4,10 +4,17 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ExplosiveConfig extends MidnightConfig {
 
-    @Entry public static boolean modEnabled = true;
+    @Comment(centered = true) public static Comment normalExplosions;
     @Entry public static boolean showBoom = true;
     @Entry public static boolean showBigExplosion = true;
     @Entry public static boolean showLingerParticles = true;
     @Entry public static boolean showDefaultExplosion = false;
+    @Comment(centered = true) public static Comment underwaterExplosions;
+    @Entry public static BubbleEnum bubbleEnum = BubbleEnum.MEDIUM;
+    public enum BubbleEnum {
+        NONE, LOW, MEDIUM, HIGH, INSANELYHIGH
+    }
+    @Comment(centered = true) public static Comment extras;
+    @Entry public static boolean modEnabled = true;
 
 }
