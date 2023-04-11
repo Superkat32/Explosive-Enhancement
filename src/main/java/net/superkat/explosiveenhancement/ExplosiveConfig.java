31c75f428c28dc01aef2bc8d2f4e6ae781559053
Superkat32 <superkat1806@gmail.com>
2023-04-10 23:33:14 -0400

    Removed MidnightLib stuff except MidnightLib itself
    REFACTOR: Removed commented out code for the config powered by MidnightLib
    REFACTOR: Removed en_us.json lines of code used for the config powered by MidnightLib

--- src/main/java/net/superkat/explosiveenhancement/ExplosiveConfig.java
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveConfig.java
@@ -18,19 +18,6 @@ public class ExplosiveConfig {
 
     public static final ConfigInstance<ExplosiveConfig> INSTANCE = new GsonConfigInstance<>(ExplosiveConfig.class, Path.of("./config/explosive-config.json"));
 
-//    @Comment(centered = true) public static Comment normalExplosions;
-//    @Entry public static boolean showBoom = true;
-//    @Entry public static boolean showBigExplosion = true;
-//    @Entry public static boolean showLingerParticles = true;
-//    @Entry public static boolean showDefaultExplosion = false;
-//    @Comment(centered = true) public static Comment underwaterExplosions;
-//    @Entry public static BubbleEnum bubbleEnum = BubbleEnum.MEDIUM;
-//    public enum BubbleEnum {
-//        NONE, LOW, MEDIUM, HIGH, INSANELYHIGH
-//    }
-//    @Comment(centered = true) public static Comment extras;
-//    @Entry public static boolean modEnabled = true;
-
     @ConfigEntry public static boolean showBoom = true;
     @ConfigEntry public static boolean showBigExplosion = true;
     @ConfigEntry public static boolean showLingerParticles = true;
