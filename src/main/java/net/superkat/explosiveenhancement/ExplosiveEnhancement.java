b3b0070de3266f1643d91cb2b36799aac720415a
Superkat32 <superkat1806@gmail.com>
2023-04-14 22:46:53 -0400

    Revert "Created branch: 1.19.2."
    This reverts commit 5a0d39f373e255d82924b842e8ce300290235c7f.

--- src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
+++ src/main/java/net/superkat/explosiveenhancement/ExplosiveEnhancement.java
@@ -4,8 +4,9 @@ import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +23,9 @@ public class ExplosiveEnhancement implements ModInitializer {
 		//Loads the config
 		MidnightConfig.init("explosiveenhancement", ExplosiveConfig.class);
 
-		Registry.register(Registry.PARTICLE_TYPE, new Identifier("explosiveenhancement", "boom"), BOOM);
-		Registry.register(Registry.PARTICLE_TYPE, new Identifier("explosiveenhancement", "big_explosion"), BIG_EXPLOSION);
-		Registry.register(Registry.PARTICLE_TYPE, new Identifier("explosiveenhancement", "linger"), LINGER);
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "boom"), BOOM);
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "big_explosion"), BIG_EXPLOSION);
+		Registry.register(Registries.PARTICLE_TYPE, new Identifier("explosiveenhancement", "linger"), LINGER);
 		LOGGER.info("Hello Fabric world!");
 	}
 }
